0d10a5736f36bfa44fb86a943631d85165a39b93
alexk <aliaksei.kuryla@innowise.com>
2024-09-09 16:36:38 +0200

    fix WEI_E8_BD

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # subgraph-leveragex
 
-Current graph version - v0.1.3
+Current graph version - v0.1.5
--- src/common.ts
+++ src/common.ts
@@ -6,6 +6,6 @@ const ONE_BI = BigInt.fromI32(1);
 export const PAIRS_STATISTIC_ENTITY_TYPE = "PairsStatistic";
 
 export const WEI_E3_BD = BigDecimal.fromString("1e3");
-export const WEI_E8_BD = BigDecimal.fromString("1e18");
+export const WEI_E8_BD = BigDecimal.fromString("1e8");
 export const WEI_E10_BD = BigDecimal.fromString("1e10");
 export const WEI_E18_BD = BigDecimal.fromString("1e18");
